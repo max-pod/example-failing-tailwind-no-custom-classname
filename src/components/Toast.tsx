80be9dbe5e6a11ae99114656d6550b6ec40bf7f1
Max Pod <github@maxpod.me>
2023-02-06 15:25:45 -0500

    First

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -36,11 +36,11 @@ export const RadixToast = ({
     <ToastPrimitive.Root
       duration={duration}
       {...props}
-      className={`relative mr-2 mt-2 flex w-80 min-w-fit max-w-[100vw] flex-col rounded-lg p-4 shadow-sm  shadow-black/25 translate-x-radix-toast-swipe-move-x radix-state-closed:animate-hide radix-state-open:animate-slideIn ${StatusColors[status]}`}
+      className={`translate-x-radix-toast-swipe-move-x radix-state-closed:animate-hide radix-state-open:animate-slideIn relative mr-2 mt-2 flex w-80 min-w-fit max-w-[100vw] flex-col  rounded-lg p-4 shadow-sm shadow-black/25 ${StatusColors[status]}`}
     >
       <ToastPrimitive.Close
         asChild
-        className="absolute top-1 right-1 rounded-lg text-2xl text-neutral-50 drop-shadow-sharp/25 hover:text-neutral-200 hover:drop-shadow-sharp/75"
+        className="drop-shadow-sharp/25 hover:drop-shadow-sharp/75 absolute top-1 right-1 rounded-lg text-2xl text-neutral-50 hover:text-neutral-200"
       >
         <RiCloseLine className="cursor-pointer" />
       </ToastPrimitive.Close>
